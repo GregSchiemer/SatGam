31d8bbdb7460fb5ed9515ab9a71d41b5febc4137
Greg Schiemer <greg@schiemer.com.au>
2026-04-17 09:59:19 +1000

    added 2 md-images to assets

--- js/gui/satgamPing.js
+++ js/gui/satgamPing.js
@@ -1,4 +1,10 @@
 // js/gui/satgamPing.js
+// A) matches the Lazzarini Ping README exactly:
+const CSOUND_URL = "https://www.unpkg.com/@csound/browser@6.18.7/dist/csound.js";
+
+// B) newer Csound 7 beta:
+// const CSOUND_URL = "https://cdn.jsdelivr.net/npm/@csound/browser@7.0.0-beta13/dist/csound.js";
+
 let csound = null;
 
 const CODE = `
@@ -6,12 +12,6 @@ instr 1
   out linenr(oscili(0dbfs*p4,p5), 0.01, p3, 0.01)
 endin
 `;
-
-// A) matches the Ping README exactly:
-const CSOUND_URL = "https://www.unpkg.com/@csound/browser@6.18.7/dist/csound.js";
-
-// B) newer Csound 7 beta (if you want to try it):
-// const CSOUND_URL = "https://cdn.jsdelivr.net/npm/@csound/browser@7.0.0-beta13/dist/csound.js";
 
 export async function pingBeep({ dur = 0.25, amp = 0.25, hz = 440 } = {}) {
   if (!csound) {
